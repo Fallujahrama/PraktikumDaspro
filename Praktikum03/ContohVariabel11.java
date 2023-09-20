d3dc8d4639d8b4eb9019a336cec99d19c06d67ac
flaxelyn <fallujahrc@gmail.com>
2023-09-20 11:51:51 +0700

    percobaan 1

---
+++ Praktikum03/ContohVariabel11.java
@@ -0,0 +1,16 @@
+public class ContohVariabel11 {
+
+    public static void main(String[]args ){
+        String hobiSaya = "Bermain video game";
+        boolean isPandai = true;
+        char jenisKelamin = 'L';
+        byte umurSaya = 19;
+        double ipk = 3.85, tinggi = 168;
+
+        System.out.println("Hobi saya adalah : " + hobiSaya);
+        System.out.println("Apakah pandai? " + isPandai);
+        System.out.println("Jenis kelamin : " + jenisKelamin);
+        System.out.println("IPK : " + ipk);
+        System.out.println(String.format("Saya berumur %s tahun dengan tinggi badan %s", umurSaya, tinggi ));
+    }
+}
