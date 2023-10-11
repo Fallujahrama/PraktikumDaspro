024e2980d3d24ad906044a270c22fa67f2a80547
flaxelyn <fallujahrc@gmail.com>
2023-10-11 11:15:22 +0700

    kabisat if

---
+++ Praktikum06/Kabisat11.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Kabisat11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int tahun;
+
+        System.out.print("Masukan tahun berapa : ");
+        tahun = sc.nextInt();
+
+        if (tahun %4 == 0 && tahun %100 != 0){
+            System.out.println("Tahun kabisat !");
+        }else {
+            System.out.println("Bukan tahun kabisat !");
+        }
+
+    }
+}
