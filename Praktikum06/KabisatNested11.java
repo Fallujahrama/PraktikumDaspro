55a1e88acef9d26368bcbeca99178c9886a70027
flaxelyn <fallujahrc@gmail.com>
2023-10-11 11:27:53 +0700

    modifikasi 400

--- Praktikum06/KabisatNested11.java
+++ Praktikum06/KabisatNested11.java
@@ -13,9 +13,12 @@ public class KabisatNested11 {
             if ((tahun %100) != 0){
                 System.out.println("Tahun kabisat !");
             } 
+            else if ((tahun %400) == 0){
+                System.out.println("Tahun kabisat !");
+            }
             else {
                 System.out.println("Bukan tahun kabisat !");
-            }
+            }   
         } else {
             System.out.println("Bukan tahun kabisat !");
         }
