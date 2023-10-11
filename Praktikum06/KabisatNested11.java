82a8161c96bd49854d0ae4921c08b085654b305f
flaxelyn <fallujahrc@gmail.com>
2023-10-11 10:50:39 +0700

    nested tahun kabisat

---
+++ Praktikum06/KabisatNested11.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class KabisatNested11 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int tahun;
+
+        System.out.print("Masukan tahun berapa : ");
+        tahun = input.nextInt();
+
+        if ((tahun %4) == 0){
+            if ((tahun %100) != 0){
+                System.out.println("Tahun kabisat !");
+            } else {
+                System.out.println("Bukan tahun kabisat !");
+            }
+        } else {
+            System.out.println("Bukan tahun kabisat !");
+        }
+    }
+}
