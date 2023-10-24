a994fad6f0346ec731b0b3ecc531fd3fb0ea9210
flaxelyn <fallujahrc@gmail.com>
2023-10-24 20:24:46 +0700

    feat: rata-rata

--- Praktikum07/ForKelipatan11.java
+++ Praktikum07/ForKelipatan11.java
@@ -4,7 +4,8 @@ public class ForKelipatan11 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int bilangan, total, counter;
+        int bilangan, total, counter, i;
+        double rata;
 
         total = 0;
         counter = 0;
@@ -12,13 +13,14 @@ public class ForKelipatan11 {
         System.out.print("Masukan bilangan 1-9 : ");
         bilangan = input.nextInt();
 
-        for (int i = 0; i < 50; i++){
+        for (i = 0; i < 50; ++i){
             if (i % bilangan == 0){
                 total += i;
-                counter++;
+                ++counter;
             }
         } 
-        System.out.printf("Pada rentang 1-50 terdapat %d bilangan kelipatan %d dengan total %d", counter, bilangan, total);
-
+        System.out.printf("Pada rentang 1-50 terdapat %d bilangan kelipatan %d dengan total %d\n", counter, bilangan, total);
+        rata = (double) total / counter ;
+        System.out.println("Rata-rata nya adalah : " + rata);
     }
 }
