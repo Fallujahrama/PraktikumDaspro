ba00ed8c43d5c605902e9e64748b185e3ce44f7a
flaxelyn <fallujahrc@gmail.com>
2023-10-25 06:31:07 +0700

    feat:i++ ditengah

---
+++ Praktikum07/WhileGaji11.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class WhileGaji11 {
+    public static void main(String[] args) {
+        Scanner input11 = new Scanner (System.in);
+
+        int jumlahKaryawan,jumlahJamLembur;
+        double gajiLembur = 0, totalGajiLembur = 0;
+        String jabatan;
+
+        System.out.print("Masukkan jumlah karyawan: ");
+        jumlahKaryawan = input11.nextInt();
+
+        int i = 0;
+        while (i < jumlahKaryawan) {
+            System.out.println("Pilihan jabatan - Direktur, Manajer, Karyawan");
+            System.out.print("Masukkan jabatan karyawan ke-" + (i+1) + ": ");
+            jabatan = input11.next();
+            System.out.print("Masukkan jumlah jam lembur: ");
+            jumlahJamLembur = input11.nextInt();
+            i++;
+
+            if(jabatan.equalsIgnoreCase ("direktur")) {
+                continue;
+            } else if (jabatan.equalsIgnoreCase("manajer")){
+                gajiLembur = jumlahJamLembur * 100000;
+            }
+            else if (jabatan.equalsIgnoreCase("karyawan")){
+                gajiLembur = jumlahJamLembur * 75000;
+            }
+            totalGajiLembur += gajiLembur;
+            // else {
+            //     System.out.println("Jabatan invalid");
+            // }
+            // i++;
+        }
+        System.out.println("Total gaji Lembur: " + totalGajiLembur);
+
+    }
+}
