0402ce1891993899fd8032d3564d42e7fdff75c3
flaxelyn <fallujahrc@gmail.com>
2023-10-28 17:41:29 +0700

    feat: copy with array

---
+++ Praktikum09/tugas1.java
@@ -0,0 +1,13 @@
+public class tugas1 {
+    public static void main(String[] args) {
+        int a [] = {1, 5, 8, 4, 3};
+        int b [] = new int [5];
+
+        for (int j = 0 ; j < b.length ; j++){
+            b[j] = a[j];
+        }
+        for (int h : b){
+            System.out.println(h);        
+        }
+    }
+}
