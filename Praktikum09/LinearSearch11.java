92b6cc59663bb546baaf37c29c616100373462ae
flaxelyn <fallujahrc@gmail.com>
2023-11-01 14:01:43 +0700

    feat: if key tidak ditemukan

--- Praktikum09/LinearSearch11.java
+++ Praktikum09/LinearSearch11.java
@@ -1,15 +1,38 @@
+import java.util.Scanner;
+
 public class LinearSearch11 {
     public static void main(String[] args) {
-        int [] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20, hasil = 0;
+        Scanner sc = new Scanner (System.in);
+        
+        int key = 0, hasil = -5, elemen;
+
+        System.out.print("Masukan jumlah elemen array : ");
+        elemen = sc.nextInt();
+        int [] arrayInt = new int[elemen];
 
         for (int i = 0; i < arrayInt.length; i++){
-            if (arrayInt[i] == key){
-                hasil = i;
+            System.out.print("Masukan elemen array ke-" +(i+1)+ " : ");
+            arrayInt[i] = sc.nextInt();
+
+        }
+
+        System.out.print("Masukan key yang ingin dicari : ");
+        key = sc.nextInt();
+
+        for (int j = 0; j < arrayInt.length; j++){
+            if (arrayInt[j] == key){
+                hasil = j;
                 break;
             }
         }
+
+        if (hasil == -5){
+            System.out.println("Key tidak ditemukan!");
+        } 
+        else{
+            System.out.println("Key ada dalam array pada posisi indeks ke-" +hasil);
+        }
         
-        System.out.println("Key ada dalam array pada posisi indeks ke-" +hasil);
+        sc.close();
     }
 }
