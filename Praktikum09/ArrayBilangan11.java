e44cdb6ebef92514f2c11410d560c5e37960f935
flaxelyn <fallujahrc@gmail.com>
2023-11-01 10:56:45 +0700

    feat: declare digabung

--- Praktikum09/ArrayBilangan11.java
+++ Praktikum09/ArrayBilangan11.java
@@ -1,11 +1,13 @@
 public class ArrayBilangan11 {
     public static void main(String[] args) {
-        int [] bilangan = new int[4];
+        // int [] bilangan = new int[4];
 
-        bilangan[0] = 5;
-        bilangan[1] = 12867;
-        bilangan[2] = -7;
-        bilangan[3] = 2000000;
+        // bilangan[0] = 5;
+        // bilangan[1] = 12867;
+        // bilangan[2] = -7;
+        // bilangan[3] = 2000000;
+
+        int [] bilangan = {5, 12867, -7, 2000000};
 
         System.out.println(bilangan[0]);
         System.out.println(bilangan[1]);
