a98fb68a5d50523a7fc095cfe14f6cdbc66fde53
flaxelyn <fallujahrc@gmail.com>
2023-11-01 10:54:42 +0700

    feat: declare dipisah

---
+++ Praktikum09/ArrayBilangan11.java
@@ -0,0 +1,16 @@
+public class ArrayBilangan11 {
+    public static void main(String[] args) {
+        int [] bilangan = new int[4];
+
+        bilangan[0] = 5;
+        bilangan[1] = 12867;
+        bilangan[2] = -7;
+        bilangan[3] = 2000000;
+
+        System.out.println(bilangan[0]);
+        System.out.println(bilangan[1]);
+        System.out.println(bilangan[2]);
+        System.out.println(bilangan[3]);
+    }
+    
+}
