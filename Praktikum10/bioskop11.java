91680c8e83b8ea23f504534ce3fbcee6ec7c9f4f
flaxelyn <fallujahrc@gmail.com>
2023-11-08 13:46:21 +0700

    feat: nama penonton pada baris ke-n foreach

--- Praktikum10/bioskop11.java
+++ Praktikum10/bioskop11.java
@@ -17,8 +17,6 @@ public class bioskop11{
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
 
         System.out.println(penonton.length);
-        
-        // System.out.println(penonton.length);
 
         // for (int i = 0; i < penonton.length; i++) {
         //     System.out.println("Panjang baris ke-" + (i+1) + " : " + penonton[i].length);
@@ -30,8 +28,12 @@ public class bioskop11{
         
         System.out.println("Penonton pada baris ke-3 : ");
 
-        for (int i = 0; i < penonton[2].length; i++) {
-            System.out.println(penonton[2][i]);
+        // for (int i = 0; i < penonton[2].length; i++) {
+        //     System.out.println(penonton[2][i]);
+        // }
+
+        for (String k : penonton[2]) {
+            System.out.println(k);
         }
 
     }
