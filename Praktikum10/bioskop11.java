aa1e5dbe37ae8afde3ee75fcd80f9cb4314a1e83
flaxelyn <fallujahrc@gmail.com>
2023-11-08 13:32:23 +0700

    feat: nama penonton baris ke-n

--- Praktikum10/bioskop11.java
+++ Praktikum10/bioskop11.java
@@ -18,9 +18,21 @@ public class bioskop11{
 
         System.out.println(penonton.length);
         
-        for (int i = 0; i < penonton.length; i++) {
-            System.out.println("Panjang baris ke-" + (i+1) + " : " + penonton[i].length);
+        // System.out.println(penonton.length);
+
+        // for (int i = 0; i < penonton.length; i++) {
+        //     System.out.println("Panjang baris ke-" + (i+1) + " : " + penonton[i].length);
+        // }
+
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris : " + barisPenonton.length);
         }
         
+        System.out.println("Penonton pada baris ke-3 : ");
+
+        for (int i = 0; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
+        }
+
     }
 }
