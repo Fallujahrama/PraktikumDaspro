8bd5409bb586ac6db79c8cc59fb9887662db787d
flaxelyn <fallujahrc@gmail.com>
2023-11-29 11:12:20 +0700

    feat: nyoba function 1

---
+++ Praktikum13/Welcome11.java
@@ -0,0 +1,10 @@
+public class Welcome11 {
+
+    public static void main(String[] args) {
+        displayMessage();
+    }
+
+    public static void displayMessage(){
+        System.out.println("Welcome to Malang State Polytechnic");
+    }
+}
